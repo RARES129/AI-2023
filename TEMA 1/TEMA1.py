2084d0a5aaabdd9241ce7f0ada6845f7135150c0
RARES129 <dascaluraresm@gmail.com>
2023-10-19 04:41:16 +0300

    Update TEMA1.py

--- TEMA 1/TEMA1.py
+++ TEMA 1/TEMA1.py
@@ -5,10 +5,10 @@
 # first_list = [1, 0, 2, 4, 5, 3, 7, 8, 6]
 
 # depth = 2
-# first_list = [1, 2, 0, 4, 5, 3, 7, 8, 6]
+first_list = [1, 2, 0, 4, 5, 3, 7, 8, 6]
 
 # depth = 1
-first_list = [1, 2, 3, 4, 5, 0, 7, 8, 6]
+# first_list = [1, 2, 3, 4, 5, 0, 7, 8, 6]
 
 # depth = 0
 # first_list = [1, 2, 3, 4, 0, 5, 6, 7, 8]
@@ -69,7 +69,6 @@ def empty_cell(current_state):
 
 
 def up(current_state):
-    print("Move up")
     index1, index2 = empty_cell(current_state)
     matrix = duplicate_matrix(current_state)
     if index1 > 0:
@@ -82,7 +81,6 @@ def up(current_state):
 
 
 def down(current_state):
-    print("Move down")
     matrix = duplicate_matrix(current_state)
     index1, index2 = empty_cell(matrix)
     if index1 < len(matrix) - 1:
@@ -95,7 +93,6 @@ def down(current_state):
 
 
 def left(current_state):
-    print("Move left")
     matrix = duplicate_matrix(current_state)
     index1, index2 = empty_cell(matrix)
     if index2 > 0:
@@ -108,7 +105,6 @@ def left(current_state):
 
 
 def right(current_state):
-    print("Move right")
     matrix = duplicate_matrix(current_state)
     index1, index2 = empty_cell(matrix)
     if index2 < len(matrix[0]) - 1:
@@ -121,25 +117,45 @@ def right(current_state):
 
 
 def algorithm_iddfs(state, depth, maximum_depth, last_state):
+    print("At depth: -> " + str(depth))
     if state is None:
         return None
     if final(state):
         print("SOLVED AT DEPTH: " + str(depth))
         print("FINAL STATE IS: " + str(list(state)))
         return True
-    print("At depth: -> " + str(depth))
     if depth == maximum_depth:
         return None
-    if not equal(state, up(state)) and not equal(last_state, up(state)):
+    if (
+        not equal(state, up(state))
+        and not equal(last_state, up(state))
+        and up(state) is not None
+    ):
+        print("Move UP")
         if algorithm_iddfs(up(state), depth + 1, maximum_depth, state):
             return True
-    if not equal(state, left(state)) and not equal(last_state, left(state)):
+    if (
+        not equal(state, left(state))
+        and not equal(last_state, left(state))
+        and left(state) is not None
+    ):
+        print("Move LEFT")
         if algorithm_iddfs(left(state), depth + 1, maximum_depth, state):
             return True
-    if not equal(state, right(state)) and not equal(last_state, right(state)):
+    if (
+        not equal(state, right(state))
+        and not equal(last_state, right(state))
+        and right(state) is not None
+    ):
+        print("Move RIGHT")
         if algorithm_iddfs(right(state), depth + 1, maximum_depth, state):
             return True
-    if not equal(state, down(state)) and not equal(last_state, down(state)):
+    if (
+        not equal(state, down(state))
+        and not equal(last_state, down(state))
+        and down(state) is not None
+    ):
+        print("Move DOWN")
         if algorithm_iddfs(down(state), depth + 1, maximum_depth, state):
             return True
 
